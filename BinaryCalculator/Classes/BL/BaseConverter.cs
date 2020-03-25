83cedb8662d366a31c0e74098b3973c4ab81aae7
Your Name <you@example.com>
2020-03-25 17:32:21 +0200

    Initial commit

---
+++ BinaryCalculator/Classes/BL/BaseConverter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BinaryCalculator.Classes.BL
+{
+    #region Enums
+    public enum NumberingSchemes
+    {
+        AToZ,
+        ZeroToZ
+    }
+    #endregion
+    public class BaseConverter
+    {
+        #region Fields & constants
+        private int _fromRadix;
+        private string _fromNumberingScheme;
+        private int _toRadix;
+        private string _toNumberingScheme;
+        private int _maxFromSchemeCharacter;
+        #endregion
+        #region Factory methods & constructors
+        private BaseConverter(int fromRadix, NumberingSchemes fromScheme, int toRadix, NumberingSchemes toScheme)
+        {
+            #region Validate arguments
+            if (fromRadix < 2 || fromRadix > 36)
+                throw new ArgumentOutOfRangeException("fromRadix", "Radix can be from 2 to 36 inclusive");
+            if (toRadix < 2 || toRadix > 36)
+                throw new ArgumentOutOfRangeException("toRadix", "Radix can be from 2 to 36 inclusive");
+            if (fromRadix > 26 && fromScheme == NumberingSchemes.AToZ)
+                throw new ArgumentOutOfRangeException("fromRadix", "Invalid numbering scheme for specified number base");
+            if (toRadix > 26 && fromScheme == NumberingSchemes.AToZ)
+                throw new ArgumentOutOfRangeException("toRadix", "Invalid numbering scheme for specified number base");
+            #endregion
+            _fromRadix = fromRadix;
+            _fromNumberingScheme = GetCharactersForNumberingScheme(fromScheme);
+            _toRadix = toRadix;
+            _toNumberingScheme = GetCharactersForNumberingScheme(toScheme);
+            _maxFromSchemeCharacter = (fromScheme == NumberingSchemes.ZeroToZ) ? fromRadix : fromRadix + 1;
+        }
+        #endregion
+        #region Public interface
+        #region Class helpers
+        public static string Convert(int fromRadix, int toRadix, string value)
+        {
+            return Convert(fromRadix, NumberingSchemes.ZeroToZ, toRadix, NumberingSchemes.ZeroToZ, value);
+        }
+        public static string Convert(int fromRadix, NumberingSchemes fromScheme, int toRadix, NumberingSchemes toScheme, string value)
+        {
+            BaseConverter BaseConverter = new BaseConverter(fromRadix, fromScheme, toRadix, toScheme);
+            return BaseConverter.Convert(value);
+        }
+        #endregion
+        #region Instance helpers
+        public string Convert(string value)
+        {
+            #region Validate arguments
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException("value");
+            #endregion
+            long temp = ConvertToBase10(value, _fromRadix, _fromNumberingScheme, _maxFromSchemeCharacter);
+            if (_toRadix == 10)
+                return temp.ToString();
+            else
+                return ConvertFromBase10(temp, _toRadix, _toNumberingScheme);
+        }
+        #endregion
+        #endregion
+        #region Helpers
+        private static string GetCharactersForNumberingScheme(NumberingSchemes scheme)
+        {
+            string characters;
+            switch (scheme)
+            {
+                case NumberingSchemes.AToZ:
+                    characters = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    break;
+                case NumberingSchemes.ZeroToZ:
+                    characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("scheme");
+            }
+            return characters;
+        }
+        private static long ConvertToBase10(string value, int fromBase, string characters, int maxFromSchemeCharacter)
+        {
+            StringBuilder fromValue = new StringBuilder(value);
+            int power = 0;
+            long result = 0;
+            while (fromValue.Length > 0)
+            {
+                int index = Array.IndexOf<char>(characters.ToCharArray(), fromValue[fromValue.Length - 1]);
+                if (index < 0)
+                    throw new FormatException("Unsupported character in value string");
+                if (index >= maxFromSchemeCharacter)
+                    throw new FormatException("Value contains character not valid for number base");
+                result += (index * (long)Math.Pow(fromBase, power));
+                if (result < 0)
+                    throw new OverflowException();
+                fromValue.Length--;
+                power++;
+            }
+            return result;
+        }
+        private static string ConvertFromBase10(long value, int toBase, string characters)
+        {
+            StringBuilder builder = new StringBuilder();
+            while (value > 0)
+            {
+                int remainder = (int)(value % toBase);
+                builder.Insert(0, characters[remainder]);
+                value /= toBase;
+            }
+            return builder.ToString();
+        }
+        #endregion
+    }
+}
---
+++ BinaryCalculator/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using BinaryCalculator.Classes.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BinaryCalculator
+{
+    public partial class MainWindow : Window
+    {
+        private char _actionState = ' ';
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeUI();
+        }
+
+        private void InitializeUI()
+        {
+            int minNumberSystemVal = 2;
+            int maxNumberSystemVal = 16;
+
+            for(int i = minNumberSystemVal; i <= maxNumberSystemVal; i++)
+            {
+                cbFirstParameterNumSystem.Items.Add(i);
+                cbSecondParameterNumSystem.Items.Add(i);
+                cbResultNumSystem.Items.Add(i);
+            }
+        }
+        private void SelectAction(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (sender is Button)
+                {
+                    var btn = sender as Button;
+                    _actionState = (btn.Tag as string)[0];
+                    foreach(Button item in (btn.Parent as Grid).Children)
+                    {
+                        if (item == btn)
+                            item.Background = new SolidColorBrush(Colors.LimeGreen);
+                        else item.Background = new SolidColorBrush(Colors.DarkBlue);
+                    }
+                    Parameter_TextChanged(null, null);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private void Parameter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(tbFirstParameter.Text != string.Empty & tbSecondParameter.Text != string.Empty && _actionState != ' ')
+            {
+                try
+                {
+                    long par1_10sys = long.Parse(BaseConverter.Convert(int.Parse(cbFirstParameterNumSystem.Text), 10, tbFirstParameter.Text));
+                    long par2_10sys = long.Parse(BaseConverter.Convert(int.Parse(cbSecondParameterNumSystem.Text), 10, tbSecondParameter.Text));
+                    long result = 0;
+
+                    switch (_actionState)
+                    {
+                        case '+': result = par1_10sys + par2_10sys; break;
+                        case '-': result = par1_10sys - par2_10sys; break;
+                        case '*': result = par1_10sys * par2_10sys; break;
+                        case '/': if (par2_10sys == 0) MessageBox.Show("На 0 делить нельзя!"); else result = par1_10sys / par2_10sys; break;
+                    }
+
+                    tbResult.Text = BaseConverter.Convert(10, int.Parse(cbResultNumSystem.Text), result.ToString());
+                }
+                catch { }
+            }
+        }
+
+        private void cbNumSystem_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Parameter_TextChanged(null, null);
+        }
+    }
+}
